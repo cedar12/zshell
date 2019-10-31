575edf626ff3fb76b4f1471b7a002b7914b3bd45
cedar12 <44419143+cedar12@users.noreply.github.com>
2019-10-31 10:00:06 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 #coding:utf-8
 import zshell,sys,os
 
-app=zshell.App({'prefix':'shell:>','error':u'错误，未找到命令{0}','ignore_case':True})
+'''
+prefix:                     命令输入显示前缀            默认：zshell:>>
+not_found_error:            不存在的命令提示信息        默认：Command not found
+args_not_match_error：      命令参数错误提示信息        默认：The number of arguments does not match
+ignore_case:                命令忽略大小写             默认：True
+
+'''
+app=zshell.App(not_found_error=u'错误，未找到命令{0}',args_not_match_error=u'参数错误')
 
 @app.shell
 def add(args):
@@ -17,15 +24,17 @@ def echo(args):
         str_arg+=i
     return str_arg
 @app.shell
-def none():
-    pass
+def none(args):
+    app.cmd(cmd='add',args=args)
+    app.cmd(cmd='ping',args=['127.0.0.1'],is_system=True)
+    return 'none 运行结束'
 @app.shell
 def order(a,b,c):
     print(a,b,c)
     pass
-@app.shell(desc='含有以下参数',args=[{'name':'status','desc':'退出状态'}])
+@app.shell(desc='退出程序命令\n\t含有以下参数',args=[{'name':'status','desc':'退出状态'},{'name':'测试','desc':'测试参数'}])
 def exit(status=0):
-    print('退出')
+    print('退出',status)
     sys.exit(0)
 
 @app.shell
@@ -39,7 +48,7 @@ def rm(f=None,r=None,_=None):
 def ls():
     list=os.listdir('./')
     for path in list:
-        print path+'\t\t'+str(os.path.getsize(path))+'字节'
+        print(path+'\t\t'+str(os.path.getsize(path))+'字节')
 
 @app.shell
 def cd(args):
